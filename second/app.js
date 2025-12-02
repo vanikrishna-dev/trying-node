0e0403940b1f0de1c4c907c9d0cbabaec322ab66
vanikrishna-dev <vanikrishna02@gmail.com>
2025-12-02 17:01:02 +0530

    5th commit

--- second/app.js
+++ second/app.js
@@ -6,29 +6,61 @@ const requestHandler = (req, res) => {
   console.log("I have made a request to the handler");
   console.log(req.method);
   res.setHeader('Content-type', 'text/html');
-  res.write(`
-    <!DOCTYPE html>
-      <html lang="en">
-      <head>
-          <title>Product Search</title>
-      </head>
-      <body>
-          <center>
-              <h1> Product Search </h1>
-          <center>
-          <p> Searching for the required products </p>
-          <form action="/products">
-              <input type="text" placeholder="Name of the product">
-              <br>
-              <br>
-              <input type="text" placeholder="Enter your budget">
-              <br>
-              <br>
-              <input type="submit">
-          </form>
-      </body>
-      </html>
-    `);
+  if(req.url==="/"){
+    res.write(`
+      <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <title>Product Search</title>
+        </head>
+        <body>
+            <center>
+                <h1> Product Search </h1>
+            <center>
+            <p> Searching for the required products </p>
+            <form action="/products">
+                <input type="text" placeholder="Name of the product">
+                <br>
+                <br>
+                <input type="text" placeholder="Enter your budget">
+                <br>
+                <br>
+                <input type="submit">
+            </form>
+        </body>
+        </html>
+      `);
+  }
+  else if (req.url === "/products?"){
+    res.write(`
+      <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <title>Product List</title>
+        </head>
+        <body>
+            <center>
+                <h1> Product List </h1>
+            <center>
+            <p> The required products will display here. </p>
+        </body>
+        </html>
+      `);
+  }
+  else{
+    res.write(`
+      <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <title>Product Search</title>
+        </head>
+        <body>
+              <h1> 404 Page Not Found </h1>
+        </body>
+        </html>
+      `);
+  }
+
     res.end();
 };
 
